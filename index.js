c1c63f112628b7dc6498be54e2aa29a6b4def295
Takuto Wada <takuto.wada@gmail.com>
2016-02-09 02:52:57 +0900

    docs(prefer-power-assert): expose rule, document configurations

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 module.exports = {
 	rules: {
+		'prefer-power-assert': require('./rules/prefer-power-assert'),
 		'test-ended': require('./rules/test-ended')
 	},
 	rulesConfig: {
+		'prefer-power-assert': 0,
 		'test-ended': 0
 	}
 };
