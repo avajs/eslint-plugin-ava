9f8b0a97770b42b0289074440ca11244008d11d9
Neil Kistner <neil.kistner@gmail.com>
2016-02-26 08:19:22 -0600

    Update to ESLint 2.0.0
    Addresses #4, #17

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ module.exports = {
 		'prefer-power-assert': require('./rules/prefer-power-assert'),
 		'test-ended': require('./rules/test-ended')
 	},
-	rulesConfig: {
-		'prefer-power-assert': 0,
-		'test-ended': 0
+	configs: {
+		recommended: {
+			env: {
+				es6: true
+			},
+			parserOptions: {
+				ecmaVersion: 6,
+				sourceType: 'module'
+			},
+			rules: {
+				'prefer-power-assert': 0,
+				'test-ended': 2
+			}
+		}
 	}
 };
