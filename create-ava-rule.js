dde1e0267d731baef57098b964161718b447866c
Takuto Wada <takuto.wada@gmail.com>
2016-02-27 02:35:20 +0900

    refactor(eslint-plugin-ava): provide minimal framework for AVA rules
    - `ava` require/import detection
    - `test` function detection
    - provide getter access to current `test` function node
    - predicate method for test modifiers
    
    fixes #12

---
+++ create-ava-rule.js
@@ -0,0 +1,133 @@
+'use strict';
+var espurify = require('espurify');
+var deepStrictEqual = require('deep-strict-equal');
+
+var avaImportDeclarationAst = {
+	type: 'ImportDeclaration',
+	specifiers: [
+		{
+			type: 'ImportDefaultSpecifier',
+			local: {
+				type: 'Identifier',
+				name: 'test'
+			}
+		}
+	],
+	source: {
+		type: 'Literal',
+		value: 'ava'
+	}
+};
+
+var avaVariableDeclaratorAst = {
+	type: 'VariableDeclarator',
+	id: {
+		type: 'Identifier',
+		name: 'test'
+	},
+	init: {
+		type: 'CallExpression',
+		callee: {
+			type: 'Identifier',
+			name: 'require'
+		},
+		arguments: [
+			{
+				type: 'Literal',
+				value: 'ava'
+			}
+		]
+	}
+};
+
+function isTestFunctionCall(node) {
+	if (node.type === 'Identifier') {
+		return node.name === 'test';
+	} else if (node.type === 'MemberExpression') {
+		return isTestFunctionCall(node.object);
+	}
+
+	return false;
+}
+
+function hasTestModifier(node, mod) {
+	if (node.type === 'CallExpression') {
+		return hasTestModifier(node.callee, mod);
+	} else if (node.type === 'MemberExpression') {
+		if (node.property.type === 'Identifier' && node.property.name === mod) {
+			return true;
+		}
+		return hasTestModifier(node.object, mod);
+	}
+	return false;
+}
+
+module.exports = function createAvaRule() {
+	var isTestFile = false;
+	var currentTestNode = null;
+
+	/* eslint quote-props: [2, "as-needed"] */
+	var predefinedRules = {
+		ImportDeclaration: function (node) {
+			if (!isTestFile && deepStrictEqual(espurify(node), avaImportDeclarationAst)) {
+				isTestFile = true;
+			}
+		},
+		VariableDeclarator: function (node) {
+			if (!isTestFile && deepStrictEqual(espurify(node), avaVariableDeclaratorAst)) {
+				isTestFile = true;
+			}
+		},
+		CallExpression: function (node) {
+			if (!currentTestNode) {
+				if (isTestFunctionCall(node.callee)) {
+					// entering test function
+					currentTestNode = node;
+				}
+			}
+		},
+		'CallExpression:exit': function (node) {
+			if (currentTestNode === node) {
+				// leaving test function
+				currentTestNode = null;
+			}
+		},
+		'Program:exit': function () {
+			isTestFile = false;
+		}
+	};
+
+	var rule = {
+		hasTestModifier: function (mod) {
+			return hasTestModifier(currentTestNode, mod);
+		},
+		merge: function (customHandlers) {
+			Object.keys(predefinedRules).forEach(function (key) {
+				var predef = predefinedRules[key];
+				if (typeof customHandlers[key] === 'function') {
+					predefinedRules[key] = function (node) {
+						if (/:exit$/.test(key)) {
+							customHandlers[key](node);
+							predef(node); // append predefined rules on exit
+						} else {
+							predef(node); // prepend predefined rules on enter
+							customHandlers[key](node);
+						}
+					};
+				}
+			});
+			return Object.assign({}, customHandlers, predefinedRules);
+		}
+	};
+	Object.defineProperty(rule, 'isTestFile', {
+		get: function () {
+			return isTestFile;
+		}
+	});
+	Object.defineProperty(rule, 'currentTestNode', {
+		get: function () {
+			return currentTestNode;
+		}
+	});
+	return rule;
+};
--- rules/prefer-power-assert.js
+++ rules/prefer-power-assert.js
@@ -1,6 +1,7 @@
 'use strict';
 var espurify = require('espurify');
 var deepStrictEqual = require('deep-strict-equal');
+var createAvaRule = require('../create-ava-rule');
 
 var notAllowed = [
 	'notOk',
@@ -11,54 +12,6 @@ var notAllowed = [
 	'regex',
 	'ifError'
 ];
-
-var avaImportDeclarationAst = {
-	type: 'ImportDeclaration',
-	specifiers: [
-		{
-			type: 'ImportDefaultSpecifier',
-			local: {
-				type: 'Identifier',
-				name: 'test'
-			}
-		}
-	],
-	source: {
-		type: 'Literal',
-		value: 'ava'
-	}
-};
-
-var avaVariableDeclaratorAst = {
-	type: 'VariableDeclarator',
-	id: {
-		type: 'Identifier',
-		name: 'test'
-	},
-	init: {
-		type: 'CallExpression',
-		callee: {
-			type: 'Identifier',
-			name: 'require'
-		},
-		arguments: [
-			{
-				type: 'Literal',
-				value: 'ava'
-			}
-		]
-	}
-};
-
-function isTestFunctionCall(node) {
-	if (node.type === 'Identifier') {
-		return node.name === 'test';
-	} else if (node.type === 'MemberExpression') {
-		return isTestFunctionCall(node.object);
-	}
-
-	return false;
-}
 
 function assertionCalleeAst(methodName) {
 	return {
@@ -105,38 +58,13 @@ function isCalleeMatched(callee, methodName) {
 
 /* eslint quote-props: [2, "as-needed"] */
 module.exports = function (context) {
-	var isTestFile = false;
-	var currentTestNode = null;
-
-	return {
-		ImportDeclaration: function (node) {
-			if (!isTestFile && deepStrictEqual(espurify(node), avaImportDeclarationAst)) {
-				isTestFile = true;
-			}
-		},
-		VariableDeclarator: function (node) {
-			if (!isTestFile && deepStrictEqual(espurify(node), avaVariableDeclaratorAst)) {
-				isTestFile = true;
-			}
-		},
+	var ava = createAvaRule();
+	return ava.merge({
 		CallExpression: function (node) {
-			if (!isTestFile) {
-				// not in test file
+			if (!ava.isTestFile || !ava.currentTestNode) {
 				return;
 			}
-
 			var callee = espurify(node.callee);
-
-			if (!currentTestNode) {
-				if (isTestFunctionCall(callee)) {
-					// entering test function
-					currentTestNode = node;
-				}
-
-				// not in test function
-				return;
-			}
-
 			if (callee.type === 'MemberExpression') {
 				notAllowed.forEach(function (methodName) {
 					if (isCalleeMatched(callee, methodName)) {
@@ -144,15 +72,6 @@ module.exports = function (context) {
 					}
 				});
 			}
-		},
-		'CallExpression:exit': function (node) {
-			if (currentTestNode === node) {
-				currentTestNode = null;
-				return;
-			}
-		},
-		'Program:exit': function () {
-			isTestFile = false;
 		}
-	};
+	});
 };
