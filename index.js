c5bb5a30984a5cca2ee560f4823cc123af3b5aea
Sindre Sorhus <sindresorhus@gmail.com>
2016-07-05 12:56:27 +0200

    simplify requiring the rules

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
 'use strict';
+const reqAll = require('req-all');
 
 module.exports = {
-	rules: {
-		'assertion-arguments': require('./rules/assertion-arguments'),
-		'assertion-message': require('./rules/assertion-message'),
-		'max-asserts': require('./rules/max-asserts'),
-		'no-cb-test': require('./rules/no-cb-test'),
-		'no-identical-title': require('./rules/no-identical-title'),
-		'no-ignored-test-files': require('./rules/no-ignored-test-files'),
-		'no-invalid-end': require('./rules/no-invalid-end'),
-		'no-only-test': require('./rules/no-only-test'),
-		'no-skip-assert': require('./rules/no-skip-assert'),
-		'no-skip-test': require('./rules/no-skip-test'),
-		'no-statement-after-end': require('./rules/no-statement-after-end'),
-		'no-todo-test': require('./rules/no-todo-test'),
-		'no-unknown-modifiers': require('./rules/no-unknown-modifiers'),
-		'prefer-power-assert': require('./rules/prefer-power-assert'),
-		'test-ended': require('./rules/test-ended'),
-		'test-title': require('./rules/test-title'),
-		'use-t-well': require('./rules/use-t-well'),
-		'use-t': require('./rules/use-t'),
-		'use-test': require('./rules/use-test'),
-		'use-true-false': require('./rules/use-true-false')
-	},
+	rules: reqAll('rules', {camelize: false}),
 	configs: {
 		recommended: {
 			env: {
