5d23800e347d88787c2c40f925dfae3caca4c05a
Sindre Sorhus <sindresorhus@gmail.com>
2018-06-24 15:37:46 +0700

    Update no-import-test-files.js

--- test/no-import-test-files.js
+++ test/no-import-test-files.js
@@ -5,37 +5,36 @@ import util from '../util';
 import rule from '../rules/no-import-test-files';
 
 const ruleTester = avaRuleTester(test, {
-	parserOptions: {
-		sourceType: 'module'
-	},
 	env: {
 		es6: true
+	},
+	parserOptions: {
+		sourceType: 'module'
 	}
 });
 
 const rootDir = path.dirname(__dirname);
+const toPath = subPath => path.join(rootDir, subPath);
 
-function toPath(subPath) {
-	return path.join(rootDir, subPath);
-}
-
-util.getAvaConfig = function () {
-	return {
-		files: ['lib/*.test.js']
-	};
-};
+util.getAvaConfig = () => ({
+	files: ['lib/*.test.js']
+});
 
 ruleTester.run('no-import-test-files', rule, {
-	valid: [{
-		code: 'import test from \'ava\';',
-	}, {
-		code: 'const test = require(\'ava\');',
-	}, {
-		code: 'console.log()',
-	}, {
-		code: 'const value = require(somePath);',
-	}],
-
+	valid: [
+		{
+			code: 'import test from \'ava\';',
+		},
+		{
+			code: 'const test = require(\'ava\');',
+		},
+		{
+			code: 'console.log()',
+		},
+		{
+			code: 'const value = require(somePath);',
+		}
+	],
 	invalid: [
 		{
 			code: 'const test = require(\'./foo.test.js\');',
