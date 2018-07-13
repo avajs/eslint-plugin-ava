161a1c2191f19541230a91e0230f39ba0c1cb5cc
Mark Wubben <mark@novemberborn.net>
2018-07-13 16:05:00 +0100

    Test new config resolving code

--- test/util.js
+++ test/util.js
@@ -1,6 +1,9 @@
+import path from 'path';
 import test from 'ava';
 import util from '../util';
 import pkg from '../package';
+
+const buildPackageFilepath = fixture => path.join(__dirname, 'fixtures', 'load-config', fixture, 'package.json');
 
 test('returns the URL of the a named rule\'s documentation', t => {
 	const url = `https://github.com/avajs/eslint-plugin-ava/blob/v${pkg.version}/docs/rules/foo.md`;
@@ -15,4 +18,47 @@ test('returns the URL of the a named rule\'s documentation at a commit hash', t 
 test('determines the rule name from the file', t => {
 	const url = `https://github.com/avajs/eslint-plugin-ava/blob/v${pkg.version}/docs/rules/util.md`;
 	t.is(util.getDocsUrl(__filename), url);
+});
+
+test('finds config in package.json', t => {
+	const conf = util.getAvaConfig(buildPackageFilepath('package-only'));
+	t.is(conf.failFast, true);
+});
+
+test('returns an empty object if both configs are present', t => {
+	t.deepEqual(util.getAvaConfig(buildPackageFilepath('package-yes-file-yes')), {});
+});
+
+test('loads config from file with `export default` syntax', t => {
+	const conf = util.getAvaConfig(buildPackageFilepath('package-no-file-yes'));
+	t.is(conf.files, 'config-file-esm-test-value');
+});
+
+test('loads config from factory function', t => {
+	const conf = util.getAvaConfig(buildPackageFilepath('package-no-file-yes-factory'));
+	t.true(conf.files.startsWith(__dirname));
+});
+
+test('returns an empty object if a config factory returns a promise', t => {
+	t.deepEqual(util.getAvaConfig(buildPackageFilepath('factory-no-promise-return')), {});
+});
+
+test('returns an empty object if a config exports a promise', t => {
+	t.deepEqual(util.getAvaConfig(buildPackageFilepath('no-promise-config')), {});
+});
+
+test('returns an empty object if a config factory does not return a plain object', t => {
+	t.deepEqual(util.getAvaConfig(buildPackageFilepath('factory-no-plain-return')), {});
+});
+
+test('returns an empty object if a config does not export a plain object', t => {
+	t.deepEqual(util.getAvaConfig(buildPackageFilepath('no-plain-config')), {});
+});
+
+test('returns an empty object if the ava.config.js file throws', t => {
+	t.deepEqual(util.getAvaConfig(buildPackageFilepath('throws')), {});
+});
+
+test('returns an empty object if a config file has no default export', t => {
+	t.deepEqual(util.getAvaConfig(buildPackageFilepath('no-default-export')), {});
 });
