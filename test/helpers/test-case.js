7b8bf348e726f0a5dd25eefb64de8af2ab629e86
Guillaume Martigny <guillaume.martigny@gmail.com>
2019-05-28 17:21:18 +0200

    Add jsDoc and puts additional params into an object

--- test/helpers/test-case.js
+++ test/helpers/test-case.js
@@ -1,27 +1,43 @@
 const header = 'const test = require(\'ava\');';
 
-function testCaseBuilder(ruleId, options = []) {
-	return function testCase(content, errorMessage = [], withoutHeader = false) {
-		const testFn = `
-			test(t => {
+/**
+ * @typedef {Function} TestCase
+ * @param {string} content - Code to test
+ * @param {Object} [config] - Set of configuration for the test
+ * @param {Array} 	[config.errorMessage] - List of error message that the rule should report
+ * @param {Boolean} [config.useHeader=true] - Don't prepend a `require` to the code, not making it a test file
+ * @param {Boolean} [config.wrapInTest=true] - Don't wrap the code inside a `test` function
+ * @returns {{code, options, errors}} Description of a test case
+ */
+/**
+ * Can be called to prepare a new test case with its rule ID and options
+ * @param {string} ruleId - Unique ID of the rule
+ * @param {*} [options] - Options for the rule
+ * @returns {TestCase} Function to call on each test case
+ */
+module.exports = (ruleId, options = []) => {
+	if (!Array.isArray(options)) {
+		options = [options];
+	}
+
+	return (content, {errorMessage = [], useHeader = true, wrapInTest = true} = {}) => {
+		const testFn = wrapInTest ? `
+			test('main', t => {
 				${content}
 			});
-		`;
+		` : content;
 
 		if (!Array.isArray(errorMessage)) {
 			errorMessage = [errorMessage];
 		}
 
 		return {
+			code: (useHeader ? header : '') + testFn,
+			options,
 			errors: errorMessage.length && errorMessage.map(message => ({
 				ruleId,
 				message
-			})),
-			options,
-			code: (withoutHeader ? '' : header) + testFn
+			}))
 		};
-	}
-}
-
-
-module.exports = testCaseBuilder;
+	};
+};
--- test/max-asserts.js
+++ test/max-asserts.js
@@ -10,8 +10,9 @@ const ruleTester = avaRuleTester(test, {
 });
 
 const ruleId = 'max-asserts';
-const basicTestCase = testCaseBuilder(ruleId);
-const lowerTestCase = testCaseBuilder(ruleId, [2]);
+const testCase = testCaseBuilder(ruleId);
+const lowAssertsCount = 2;
+const lowerTestCase = testCaseBuilder(ruleId, lowAssertsCount);
 const errorMessage = (expected, found) => `Expected at most ${expected} assertions, but found ${found}.`;
 
 function nbAssertions(n) {
@@ -20,43 +21,48 @@ function nbAssertions(n) {
 
 ruleTester.run('max-asserts', rule, {
 	valid: [
-		basicTestCase(`test(t => { ${nbAssertions(3)} });`),
-		basicTestCase(`
-			test(t => { ${nbAssertions(3)} });
-			test(t => { ${nbAssertions(3)} });
-		`),
-		basicTestCase(`test(t => { t.plan(5); ${nbAssertions(5)} });`),
-		basicTestCase(`test(t => { t.is.skip(1, 1); ${nbAssertions(4)} });`),
-		basicTestCase(`test.cb(t => { ${nbAssertions(5)} t.end(); });`),
-		basicTestCase(`test(t => { t.context.bar(); ${nbAssertions(5)} });`),
-		basicTestCase(`test(t => { ${'t.context.is(1, 1); '.repeat(6)}});`),
-		basicTestCase(`test(t => { ${'foo.t.is(1, 1); '.repeat(6)}});`),
+		testCase(nbAssertions(3)),
+		testCase(`
+			test('1', t => { ${nbAssertions(3)} });
+			test('2', t => { ${nbAssertions(3)} });
+		`, {wrapInTest: false}),
+		testCase(`t.plan(5); ${nbAssertions(5)}`),
+		testCase(`t.is.skip(1, 1); ${nbAssertions(4)}`),
+		testCase(`test.cb('main', t => { ${nbAssertions(5)} t.end(); });`, {wrapInTest: false}),
+		testCase(`t.context.bar(); ${nbAssertions(5)}`),
+		testCase('t.context.is(1, 1); '.repeat(6)),
+		testCase('foo.t.is(1, 1); '.repeat(6)),
 		// Shouldn't be triggered since it's not a test file
-		basicTestCase(`test(t => { ${nbAssertions(10)} });`, undefined, true),
+		testCase(nbAssertions(10), {useHeader: false}),
 
-		lowerTestCase(`test(t => { ${nbAssertions(2)} });`),
-		lowerTestCase(`test(t => { notT.is(1, 1); notT.is(1, 1); notT.is(1, 1); });`)
+		lowerTestCase(nbAssertions(2)),
+		lowerTestCase('notT.is(1, 1);'.repeat(3))
 	],
 	invalid: [
-		basicTestCase(`test(t => { ${nbAssertions(6)} });`, errorMessage(5, 6)),
-		basicTestCase(`test(t => { ${nbAssertions(10)} });`, errorMessage(5, 10)),
-		basicTestCase(`
+		testCase(nbAssertions(6), {errorMessage: errorMessage(5, 6)}),
+		testCase(nbAssertions(10), {errorMessage: errorMessage(5, 10)}),
+		testCase(`
 			test(t => { ${nbAssertions(3)} });
 			test(t => { ${nbAssertions(6)} });
-		`, errorMessage(5, 6)),
-		basicTestCase(`test(t => { t.plan(5); ${nbAssertions(6)} });`, errorMessage(5, 6)),
-		basicTestCase(`test(t => { t.skip.is(1, 1); ${nbAssertions(5)} });`, errorMessage(5, 6)),
-		basicTestCase(`test.cb(t => { ${nbAssertions(6)} t.end(); });`, errorMessage(5, 6)),
-		basicTestCase(`test.cb(t => { ${nbAssertions(6)} t.end(); });`, errorMessage(5, 6)),
+		`, {errorMessage: errorMessage(5, 6), wrapInTest: false}),
+		testCase(`t.plan(5); ${nbAssertions(6)}`, {errorMessage: errorMessage(5, 6)}),
+		testCase(`t.skip.is(1, 1); ${nbAssertions(5)}`, {errorMessage: errorMessage(5, 6)}),
+		testCase(`test.cb('main', t => { ${nbAssertions(6)} t.end(); });`, {
+			errorMessage: errorMessage(5, 6),
+			wrapInTest: false
+		}),
 
-		lowerTestCase(`test(t => { ${nbAssertions(3)} });`, errorMessage(2, 3)),
+		lowerTestCase(nbAssertions(3), {errorMessage: errorMessage(2, 3)}),
 
-		basicTestCase(`
-			test(t => { ${nbAssertions(6)} });
-			test(t => { ${nbAssertions(10)} });
-		`, [
-			errorMessage(5, 6),
-			errorMessage(5, 10) // Should have two errors, one per test
-		])
+		testCase(`
+			test('1', t => { ${nbAssertions(6)} });
+			test('2', t => { ${nbAssertions(10)} });
+		`, {
+			errorMessage: [
+				errorMessage(5, 6),
+				errorMessage(5, 10) // Should have two errors, one per test
+			],
+			wrapInTest: false
+		})
 	]
 });
