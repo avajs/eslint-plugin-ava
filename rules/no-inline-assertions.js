c60485023bac7cd49ec2c9cebacc4dfdf9120641
Huáng Jùnliàng <i@jhuang.me>
2019-06-13 21:18:10 -0400

    feat: `no-inline-assertions` rule

---
+++ rules/no-inline-assertions.js
@@ -0,0 +1,61 @@
+'use strict';
+const {visitIf} = require('enhance-visitors');
+const createAvaRule = require('../create-ava-rule');
+const util = require('../util');
+
+function report({ node, context }) {
+	context.report({
+		node: node,
+		message: 'Assertions should not be called from an inline function'
+	})
+}
+const create = context => {
+	const ava = createAvaRule();
+
+	return ava.merge({
+		CallExpression: visitIf([
+			ava.isInTestFile,
+			ava.isTestNode
+		])(node => {
+			const functionArgIndex = node.arguments.length - 1;
+			if (functionArgIndex > 1) {
+				return;
+			}
+
+			const functionArg = node.arguments[functionArgIndex];
+
+			if (!util.isFunctionExpression(functionArg)) {
+				return;
+			}
+
+			const { body } = functionArg;
+			if (body.type === "CallExpression") {
+				report({ node, context });
+				return;
+			}
+
+			if (body.type === "BlockStatement") {
+				if (body.loc.start.line !== body.loc.end.line) {
+					return;
+				}
+
+				if (!body.body.length) {
+					return;
+				}
+
+				report({ node, context });
+			}
+
+		})
+	});
+};
+
+module.exports = {
+	create,
+	meta: {
+		docs: {
+			url: util.getDocsUrl(__filename)
+		},
+		type: 'suggestion'
+	}
+};
---
+++ test/no-inline-assertions.js
@@ -0,0 +1,58 @@
+import test from 'ava';
+import avaRuleTester from 'eslint-ava-rule-tester';
+import rule from '../rules/no-inline-assertions';
+
+const ruleTester = avaRuleTester(test, {
+	env: {
+		es6: true
+	}
+});
+
+const errors = [{ruleId: 'no-inline-assertions'}];
+const header = 'const test = require(\'ava\');\n';
+
+ruleTester.run('no-todo-test', rule, {
+	valid: [
+		header + 'test("my test name", t => {\n t.true(fn()); \n});',
+		// Shouldn't be triggered since test body is empty
+		header + 'test("my test name", () => {});',
+		header + 'test("my test name", (t) => {});',
+		// Shouldn't be triggered since test body is ill-defined
+		header + 'test("my test name", (t) => "foo");',
+		// Shouldn't be triggered since it's not a test file
+		'test.todo("my test name");',
+		// Shouldn't be triggered since the signature is incorrect
+		header + 'test.todo("my test name", "bar");',
+		header + 'test.todo("my test name", undefined, t => {})'
+	],
+	invalid: [
+		{
+			code: header + 'test("my test name", t => { t.skip(); });',
+			errors
+		},
+		{
+			code: header + 'test("my test name", t => t.skip());',
+			errors
+		},
+		{
+			code: header + 'test("my test name", t => t.true(fn()));',
+			errors
+		},
+		{
+			code: header + 'test("my test name", t => \n t.true(fn()));',
+			errors
+		},
+		{
+			code: header + 'test("my test name", t => { t.true(fn()) });',
+			errors
+		},
+		{
+			code: header + 'test("my test name", function(t) { foo(); });',
+			errors
+		},
+		{
+			code: header + 'test("my test name", t => { return t.true(fn()) });',
+			errors
+		}
+	]
+});
