e30eafa67b0ac156c42ebc7ce3119afcace08c34
Alex Zherdev <alex_zherdev@acl.com>
2019-09-16 06:54:43 -0700

    Support named imports and destructured requires (#232)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

---
+++ test/create-ava-rule.js
@@ -0,0 +1,71 @@
+import test from 'ava';
+import avaRuleTester from 'eslint-ava-rule-tester';
+import {visitIf} from 'enhance-visitors';
+import createAvaRule from '../create-ava-rule';
+
+const rule = {
+	create: context => {
+		const ava = createAvaRule();
+
+		return ava.merge({
+			'Program:exit': node => {
+				if (!ava.isInTestFile()) {
+					context.report({node, message: 'not a test file'});
+				}
+			}
+		});
+	}
+};
+
+const ruleTester = avaRuleTester(test, {
+	env: {
+		es6: true,
+	},
+	parserOptions: {
+		sourceType: 'module'
+	}
+});
+
+const errors = [
+	{
+		message: 'not a test file'
+	}
+];
+
+ruleTester.run('rule-fixture', rule, {
+	valid: [
+		'const test = require(\'ava\');',
+		'const {serial} = require(\'ava\');',
+		'const {serial: test} = require(\'ava\');',
+		'import test from \'ava\';',
+		'import {serial} from \'ava\';',
+		'import {serial as test} from \'ava\';'
+	],
+
+	invalid: [
+		{
+			code: 'const test2 = require(\'ava\');',
+			errors
+		},
+		{
+			code: 'const {serial2} = require(\'ava\');',
+			errors
+		},
+		{
+			code: 'const {serial2: test} = require(\'ava\');',
+			errors
+		},
+		{
+			code: 'import test2 from \'ava\';',
+			errors
+		},
+		{
+			code: 'import {serial2} from \'ava\';',
+			errors
+		},
+		{
+			code: 'import {serial2 as test} from \'ava\';',
+			errors
+		}
+	]
+});
