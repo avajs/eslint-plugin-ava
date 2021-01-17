45aa6f950065ac81444609f984f467c985ad9c1f
Boomber <boombercmd123@gmail.com>
2021-01-17 17:04:23 +0900

    Detect more assertions in `prefer-t-regex` (#319)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- rules/prefer-t-regex.js
+++ rules/prefer-t-regex.js
@@ -13,10 +13,165 @@ const create = context => {
 		'falsy'
 	]);
 
+	const equalityTests = new Set([
+		'is',
+		'deepEqual'
+	]);
+
+	// Find the latest reference to the given identifier's name.
 	const findReference = name => {
 		const reference = context.getScope().references.find(reference => reference.identifier.name === name);
-		const definitions = reference.resolved.defs;
-		return definitions[definitions.length - 1].node;
+
+		if (reference && reference.resolved) {
+			const definitions = reference.resolved.defs;
+
+			if (definitions.length === 0) {
+				return;
+			}
+
+			return definitions[definitions.length - 1].node;
+		}
+	};
+
+	/*
+	Recursively find the "origin" node of the given node.
+
+	Note: `context.getScope()` doesn't contain information about the outer scope so in most cases this function will only find the reference directly above the current scope. So the following code will only find the reference in this order: y -> x, and it will have no knowledge of the number `0`. (assuming we run this function on the identifier `y`)
+
+	```
+	const test = require('ava');
+
+	let x = 0;
+	let y = x;
+
+	test(t => {
+		t.is(y, 0);
+	});
+	```
+	*/
+	const findRootReference = node => {
+		if (node.type === 'Identifier') {
+			const reference = findReference(node.name);
+
+			if (reference && reference.init) {
+				return findRootReference(reference.init);
+			}
+
+			return node;
+		}
+
+		if (node.type === 'CallExpression' || node.type === 'NewExpression') {
+			return findRootReference(node.callee);
+		}
+
+		if (node.type === 'MemberExpression') {
+			return findRootReference(node.object);
+		}
+
+		return node;
+	};
+
+	/*
+	Determine if the given node is a regex expression.
+
+	There are two ways to create regex expressions in JavaScript: Regex literal and `RegExp` class.
+	1. Regex literal can be easily looked up using the `.regex` property on the node.
+	2. `RegExp` class can't be looked up so the function just checks for the name `RegExp`.
+	*/
+	const isRegExp = lookup => {
+		if (lookup.regex) {
+			return true;
+		}
+
+		// Look up references in case it's a variable or RegExp declaration.
+		const reference = findRootReference(lookup);
+		return reference.regex || reference.name === 'RegExp';
+	};
+
+	const booleanHandler = node => {
+		const firstArg = node.arguments[0];
+
+		const isFunctionCall = firstArg.type === 'CallExpression';
+		if (!isFunctionCall || !firstArg.callee.property) {
+			return;
+		}
+
+		const {name} = firstArg.callee.property;
+		let lookup = {};
+		let variable = {};
+
+		if (name === 'test') {
+			// Represent: `lookup.test(variable)`
+			lookup = firstArg.callee.object;
+			variable = firstArg.arguments[0];
+		} else if (['search', 'match'].includes(name)) {
+			// Represent: `variable.match(lookup)`
+			lookup = firstArg.arguments[0];
+			variable = firstArg.callee.object;
+		}
+
+		if (!isRegExp(lookup)) {
+			return;
+		}
+
+		const assertion = ['true', 'truthy'].includes(node.callee.property.name) ? 'regex' : 'notRegex';
+
+		const fix = fixer => {
+			const source = context.getSourceCode();
+			return [
+				fixer.replaceText(node.callee.property, assertion),
+				fixer.replaceText(firstArg, `${source.getText(variable)}, ${source.getText(lookup)}`)
+			];
+		};
+
+		context.report({
+			node,
+			message: `Prefer using the \`t.${assertion}()\` assertion.`,
+			fix
+		});
+	};
+
+	const equalityHandler = node => {
+		const [firstArg, secondArg] = node.arguments;
+
+		const firstArgumentIsRegex = isRegExp(firstArg);
+		const secondArgumentIsRegex = isRegExp(secondArg);
+
+		// If both are regex, or neither are, the expression is ok.
+		if (firstArgumentIsRegex === secondArgumentIsRegex) {
+			return;
+		}
+
+		const matchee = secondArgumentIsRegex ? firstArg : secondArg;
+		const regex = secondArgumentIsRegex ? secondArg : firstArg;
+
+		const booleanFixer = assertion => fixer => {
+			const source = context.getSourceCode();
+			return [
+				fixer.replaceText(node.callee.property, assertion),
+				fixer.replaceText(firstArg, `${source.getText(regex.arguments[0])}`),
+				fixer.replaceText(secondArg, `${source.getText(regex.callee.object)}`)
+			];
+		};
+
+		// Only fix a statically verifiable equality.
+		if (regex && matchee.type === 'Literal') {
+			let assertion;
+
+			if (matchee.raw === 'true') {
+				assertion = 'regex';
+			} else if (matchee.raw === 'false') {
+				assertion = 'notRegex';
+			} else {
+				return;
+			}
+
+			context.report({
+				node,
+				message: `Prefer using the \`t.${assertion}()\` assertion.`,
+				fix: booleanFixer(assertion)
+			});
+		}
 	};
 
 	return ava.merge({
@@ -24,64 +179,20 @@ const create = context => {
 			ava.isInTestFile,
 			ava.isInTestNode
 		])(node => {
-			// Call a boolean assertion, for example, `t.true`, `t.false`, …
-			const isBooleanAssertion = node.callee.type === 'MemberExpression' &&
-				booleanTests.has(node.callee.property.name) &&
+			const isAssertion = node.callee.type === 'MemberExpression' &&
 				util.getNameOfRootNodeObject(node.callee) === 't';
 
-			if (!isBooleanAssertion) {
-				return;
+			const isBooleanAssertion = isAssertion &&
+				booleanTests.has(node.callee.property.name);
+
+			const isEqualityAssertion = isAssertion &&
+				equalityTests.has(node.callee.property.name);
+
+			if (isBooleanAssertion) {
+				booleanHandler(node);
+			} else if (isEqualityAssertion) {
+				equalityHandler(node);
 			}
-
-			const firstArg = node.arguments[0];
-
-			// First argument is a call expression
-			const isFunctionCall = firstArg.type === 'CallExpression';
-			if (!isFunctionCall || !firstArg.callee.property) {
-				return;
-			}
-
-			const {name} = firstArg.callee.property;
-			let lookup = {};
-			let variable = {};
-
-			if (name === 'test') {
-				// `lookup.test(variable)`
-				lookup = firstArg.callee.object;
-				variable = firstArg.arguments[0];
-			} else if (['search', 'match'].includes(name)) {
-				// `variable.match(lookup)`
-				lookup = firstArg.arguments[0];
-				variable = firstArg.callee.object;
-			}
-
-			let isRegExp = lookup.regex;
-
-			// It's not a regexp but an identifier
-			if (!isRegExp && lookup.type === 'Identifier') {
-				const reference = findReference(lookup.name);
-				isRegExp = reference.init.regex;
-			}
-
-			if (!isRegExp) {
-				return;
-			}
-
-			const assertion = ['true', 'truthy'].includes(node.callee.property.name) ? 'regex' : 'notRegex';
-
-			const fix = fixer => {
-				const source = context.getSourceCode();
-				return [
-					fixer.replaceText(node.callee.property, assertion),
-					fixer.replaceText(firstArg, `${source.getText(variable)}, ${source.getText(lookup)}`)
-				];
-			};
-
-			context.report({
-				node,
-				message: `Prefer using the \`t.${assertion}()\` assertion.`,
-				fix
-			});
 		})
 	});
 };
